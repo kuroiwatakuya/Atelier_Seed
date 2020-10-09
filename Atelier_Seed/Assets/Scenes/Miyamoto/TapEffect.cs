befec677afb5bc8870dd5f6da0f19ccabe9508d0
MiyamotoSaki <miyamoto.hal@gmail.com>
2020-10-09 17:06:05 +0900

    宮本：タップエフェクトの実装と点滅スクリプト調整。

---
+++ Atelier_Seed/Assets/Scenes/Miyamoto/TapEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapEffect : MonoBehaviour
+{
+    private GameObject TapEffectObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        TapEffectObject = (GameObject)Resources.Load("TapEffect_RandS");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // マウスカーソル位置取得        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 MousePos = Input.mousePosition;
+            MousePos.z = 5.0f;
+
+            Vector3 EffectPos = Camera.main.ScreenToWorldPoint(MousePos);
+
+
+            Instantiate(TapEffectObject, new Vector3(EffectPos.x, EffectPos.y, EffectPos.z), Quaternion.identity);
+        }
+    }
+}
