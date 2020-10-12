143f3cb72148098f36afcd1e327b93b1f292efba
MiyamotoSaki <miyamoto.hal@gmail.com>
2020-10-12 20:34:51 +0900

    Revert "プレイヤーが壁にぶつかった時にむにゅってなるシステムを実装"
    This reverts commit 1e8ef77d2da01385d4c4b6b3145bbdd344daf8dd.

--- Atelier_Seed/Assets/Scenes/Miyamoto/CJellyBound.cs
+++ Atelier_Seed/Assets/Scenes/Miyamoto/CJellyBound.cs
@@ -25,34 +25,39 @@ public class CJellyBound : MonoBehaviour
     // //   横に潰れる（横長になる）関数                 // //
     // //                                                // //
     // //   〇 引数 〇                                   // //
-    // //   scale    ：変形させるもののScale             // //
+    // //   transform：変形させるもののtransform         // //
     // //   min_scale：変形後の大きさの値                // //
     // //   change   ：一回の処理でどれくらい潰すかの量  // //
     // //                                                // //
     // //   〇 戻り値 〇                                 // //
-    // //   変形した後のScale                            // //
+    // //   変形した後のtransform                        // //
     // //                                                // //
 
     //----------------------------------------------------------------------------------------------------
 
-    public static Vector3 Crush_Height(Vector3 scale, float min_scale, float change)
+    public static Transform Crash_Height(Transform transform, float min_scale, float change)
     {
-        // 変形させる拡大縮小値を取得
-        Vector3 tempScale = scale;
+        // 拡大縮小値を取得
+        Vector3 scale = transform.localScale;
 
 
-        // ｙ値を変化量分潰す
-        tempScale.y -= change;
+        // 拡大縮小のｙ値を変化量分潰す
+        scale.y -= change;
 
-        // 潰しすぎないように調整
-        if (tempScale.y < min_scale)
+
+        // 変形後の大きさの値（最低値）より小さくならないように設定する
+        if(scale.y < min_scale)
         {
-            tempScale.y = min_scale;
+            scale.y = min_scale;
         }
 
 
-        // 変形させた拡大縮小値を返す
-        return tempScale;
+        // 取得したトランスフォームの拡大縮小値を変形後の値に設定
+        transform.localScale = scale;
+
+
+        // 変形したトランスフォームを返す
+        return transform;
     }
 
 
@@ -63,34 +68,39 @@ public class CJellyBound : MonoBehaviour
     // //   縦に潰れる（縦長になる）関数                  // //
     // //                                                 // //
     // //   〇 引数 〇                                    // //
-    // //   scale    ：変形させるもののScale              // //
+    // //   transform：変形させるもののtransform          // //
     // //   min_scale：変形後の大きさの値                 // //
     // //   change 　：一回の処理でどれくらい潰すかの量   // //
     // //                                                 // //
     // //   〇 戻り値 〇                                  // //
-    // //   変形した後のScale                             // //
+    // //   変形した後のtransform                         // //
     // //                                                 // //
 
     //----------------------------------------------------------------------------------------------------
 
-    public static Vector3 Crush_Width(Vector3 scale, float min_scale, float change)
+    public static Transform Crash_Width(Transform transform, float min_scale, float change)
     {
-        // 変形させる拡大縮小値を取得
-        Vector3 tempScale = scale;
+        // 拡大縮小値を取得
+        Vector3 scale = transform.localScale;
 
 
-        // ｘ値を変化量分潰す
-        tempScale.x -= change;
+        // 拡大縮小のｘ値を変化量分潰す
+        scale.x -= change;
 
-        // 潰しすぎないように調整
-        if (tempScale.x < min_scale)
+        
+        // 変形後の大きさの値（最低値）より小さくならないように設定する
+        if (scale.x < min_scale)
         {
-            tempScale.x = min_scale;
+            scale.x = min_scale;
         }
 
 
-        // 変形させた拡大縮小値を返す
-        return tempScale;
+        // 取得したトランスフォームの拡大縮小値を変形後の値に設定
+        transform.localScale = scale;
+
+
+        // 変形したトランスフォームを返す
+        return transform;
     }
 
 
@@ -101,34 +111,39 @@ public class CJellyBound : MonoBehaviour
     // //   縦に伸ばす関数                                 // //
     // //                                                  // //
     // //   〇 引数 〇                                     // //
-    // //   scale    ：変形させるもののScale               // //
+    // //   transform：変形させるもののtransform           // //
     // //   max_scale：変形後の大きさの値                  // //
     // //   change   ：一回の処理でどれくらい伸ばすかの量  // //
     // //                                                  // //
     // //   〇 戻り値 〇                                   // //
-    // //   変形した後のScale                              // //
+    // //   変形したtransform                              // //
     // //                                                  // //
 
     //----------------------------------------------------------------------------------------------------
 
-    public static Vector3 Expand_Height(Vector3 scale, float max_scale, float change)
+    public static Transform Expand_Height(Transform transform, float max_scale, float change)
     {
-        // 変形させる拡大縮小値を取得
-        Vector3 tempScale = scale;
+        // 拡大縮小値を取得
+        Vector3 scale = transform.localScale;
 
 
-        // ｙ値を変化量分伸ばす
-        tempScale.y += change;
+        // 拡大縮小のｙ値を変化量分伸ばす
+        scale.y += change;
 
-        // 伸ばしすぎないように調整
-        if (tempScale.y > max_scale)
+
+        // 変形後の大きさの値（最大値）より大きくならないように設定する
+        if (scale.y > max_scale)
         {
-            tempScale.y = max_scale;
+            scale.y = max_scale;
         }
 
 
-        // 変形させた拡大縮小値を返す
-        return tempScale;
+        // 取得したトランスフォームの拡大縮小値を変形後の値に設定
+        transform.localScale = scale;
+
+
+        // 変形したトランスフォームを返す
+        return transform;
     }
 
 
@@ -139,34 +154,39 @@ public class CJellyBound : MonoBehaviour
     // //   横に伸ばす関数                                    // //
     // //                                                     // //
     // //   〇 引数 〇                                        // //
-    // //   scale    ：変形させるもののScale                  // //
+    // //   transform：変形させるもののtransform              // //
     // //   max_scale：変形後の大きさの値                     // // 
     // //   change   ：一回の処理でどれくらい伸ばすかの量     // //
     // //                                                     // //
     // //   〇 戻り値 〇                                      // //
-    // //   変形した後のScale                                 // //
+    // //   変形した後のtransform                             // //
     // //                                                     // //
 
     //----------------------------------------------------------------------------------------------------
 
-    public static Vector3 Expand_Width(Vector3 scale, float max_scale, float change)
+    public static Transform Expand_Width(Transform transform, float max_scale, float change)
     {
-        // 変形させる拡大縮小値を取得
-        Vector3 tempScale = scale;
+        // 拡大縮小値を取得
+        Vector3 scale = transform.localScale;
 
 
-        // ｘ値を変化量分伸ばす
-        tempScale.x += change;
+        // 拡大縮小のｘ値を変化量分伸ばす
+        scale.x += change;
 
-        // 伸ばしすぎないように調整
-        if (tempScale.x > max_scale)
+
+        // 変形後の大きさの値（最大値）より大きくならないように設定する
+        if (scale.x > max_scale)
         {
-            tempScale.x = max_scale;
+            scale.x = max_scale;
         }
 
 
-        // 変形させた拡大縮小値を返す
-        return tempScale;
+        // 取得したトランスフォームの拡大縮小値を変形後の値に設定
+        transform.localScale = scale;
+
+
+        // トランスフォームを返す
+        return transform;
     }
     
 }
