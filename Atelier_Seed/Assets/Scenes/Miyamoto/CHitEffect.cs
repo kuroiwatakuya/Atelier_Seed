82c9af2fb04dfff19d57fe712dd4fe7c0b7bfb0d
MiyamotoSaki <miyamoto.hal@gmail.com>
2020-10-13 16:03:41 +0900

    トロフィー取得時と壁衝突時のエフェクトを作成

---
+++ Atelier_Seed/Assets/Scenes/Miyamoto/CHitEffect.cs
@@ -0,0 +1,48 @@
+﻿
+// //                        // //
+// //   Author:宮本 早希     // //
+// //   衝突エフェクト生成   // //
+// //                        // //
+
+
+// // インクルードファイル的なやつ // //
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// // クラス // //
+public class CHitEffect : MonoBehaviour
+{
+    // 衝突エフェクト格納用ゲームオブジェクト
+    private GameObject HitEffectObject;
+
+
+    // エフェクトの座標
+    private Vector3 Effpos;
+
+
+    // // 初期化 // //
+    void Start()
+    {
+        // 衝突エフェクトを検索して代入
+        HitEffectObject = (GameObject)Resources.Load("HitEffect");
+    }
+
+
+    // // 更新 // //
+    void Update()
+    {
+        Effpos = this.transform.position;        
+    }
+
+
+    // // 当たった時 // //
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "PlayerCrush")
+        {
+            Instantiate(HitEffectObject, new Vector3(Effpos.x, Effpos.y, Effpos.z), Quaternion.identity);
+        }
+    }
+}
---
+++ Atelier_Seed/Assets/Scenes/Miyamoto/CTapEffect.cs
@@ -0,0 +1,47 @@
+﻿
+// //                               // //
+// //   Author:宮本 早希            // //
+// //   タップエフェクト呼出関数    // //
+// //                               // //
+
+
+// // インクルードファイル的なやつ // //
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// // クラス // //
+public class CTapEffect : MonoBehaviour
+{
+    // タップエフェクトプレハブ格納用ゲームオブジェクト
+    private GameObject TapEffectObject;
+
+
+    // // 初期化 // //
+    void Start()
+    {
+        // タップエフェクトプレハブを検索して代入
+        TapEffectObject = (GameObject)Resources.Load("TapEffect_RandS");
+    }
+
+
+    // // 更新 // //
+    void Update()
+    {
+        // マウスカーソル位置取得
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 MousePos = Input.mousePosition;     // マウス座標を取得
+            MousePos.z = 5.0f;                          // ｚ座標値補間
+
+
+            // マウスカーソルがいるところの座標をエフェクト座標として設定
+            Vector3 EffectPos = Camera.main.ScreenToWorldPoint(MousePos);
+
+
+            // エフェクトを生成
+            Instantiate(TapEffectObject, new Vector3(EffectPos.x, EffectPos.y, EffectPos.z), Quaternion.identity);
+        }
+    }
+}
