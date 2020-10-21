6fbe95938377c5a0f206890c068ef59e7862bdc3
sunghee469 <sungheelee004@gmail.com>
2020-10-21 16:56:47 +0900

    プレイヤー復旧

--- Atelier_Seed/Assets/Scenes/Sonfi/CPlayerScript.cs
+++ Atelier_Seed/Assets/Scenes/Sonfi/CPlayerScript.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Common;
 
 //**************************************
 // プレイヤー全般のスクリプト
@@ -24,6 +25,8 @@ public class CPlayerScript : MonoBehaviour
     private Vector3 EffectPosition;                         // エフェクト生成座標
     private GameObject GunFind;                             // Gun発見用オブジェクト
     private Transform ShotPool;                             // オブジェクト保存用空オブジェクトのtransform
+    private Transform GunEnterPool;                         // 同上（Gun進入用）
+    private Transform GunShootPool;                         // 同上（Gun発射用）
     //---宮本加筆ここまで------------------------------
 
 
@@ -101,6 +104,20 @@ public class CPlayerScript : MonoBehaviour
     //大砲入る条件用
     public bool GunTrophyFlag;
 
+    //かべぶつかったか
+    public bool WallFlag;
+    //壁にぶつかった回数
+    public int WallCount;
+    //飛ぶ
+    public bool Fly;
+    //飛び始めた座標
+    private Vector2 StartPosition;
+    //風衝突
+    public bool Wind;
+
+    //取得コイン
+    public int GetCoin;
+
     // Start is called before the first frame update
 
     //アニメーション用変数
@@ -141,7 +158,12 @@ public class CPlayerScript : MonoBehaviour
 
         StopFieldCount = 0;
 
+        WallCount = 0;
+
         GetStageTrophy = false;
+
+        Fly = false;
+
 
         //---宮本加筆ここから------------------------------
         ShotEffect = (GameObject)Resources.Load("Effect_PlayerShot");   // プレイヤーショットエフェクトセット
@@ -151,7 +173,9 @@ public class CPlayerScript : MonoBehaviour
         {
             GunFind = GameObject.FindWithTag("Gun");                    // Gunオブジェクト検索
         }
-        ShotPool = new GameObject("Shot").transform;                    // ショット関係エフェクトオブジェクト生成
+        ShotPool = new GameObject("PlayerShot").transform;              // プレイヤーショットエフェクトオブジェクト生成
+        GunEnterPool = new GameObject("GunEntry").transform;            // Gunに入った時のエフェクトオブジェクト生成
+        GunShootPool = new GameObject("GunShoot").transform;            // Gun射出時のエフェクトオブジェクト生成
         //---宮本加筆ここまで------------------------------
 
     }
@@ -235,6 +259,8 @@ public class CPlayerScript : MonoBehaviour
                 //クリックフラグがオンなら
                 if (ClickFlag)
                 {
+                    StartPosition = transform.position;
+
                     ClickFlag = false;
 
 
@@ -251,7 +277,7 @@ public class CPlayerScript : MonoBehaviour
 
                         //---宮本加筆ここから------------------------------
                         // ショットエフェクト発生
-                        GetObject(ShotEffect, EffectPosition, Quaternion.identity);
+                        GetPlayerShotObject(ShotEffect, EffectPosition, Quaternion.identity);
                         //---宮本加筆ここまで------------------------------
 
                         //矢印オフ
@@ -344,6 +370,7 @@ public class CPlayerScript : MonoBehaviour
                     //クリックフラグがオンなら
                     if (ClickFlag)
                     {
+                        StartPosition = transform.position;
                         ClickFlag = false;
 
                         if (DirectionForce.magnitude >= MinMagnitude)
@@ -359,7 +386,7 @@ public class CPlayerScript : MonoBehaviour
 
                             //---宮本加筆ここから------------------------------
                             // ショットエフェクト発生
-                            GetObject(ShotEffect, EffectPosition, Quaternion.identity);
+                            GetPlayerShotObject(ShotEffect, EffectPosition, Quaternion.identity);
                             //---宮本加筆ここまで------------------------------
 
                             //矢印オフ
@@ -383,6 +410,9 @@ public class CPlayerScript : MonoBehaviour
         //遅くなったらとめる
         if (Velocity.y == 0 && Velocity.x <= 12 && Velocity.x >= -12 && PlayFlag && !GunFlag)
         {
+            //連続壁激突
+            WallCount = 0;
+
             Rbody.velocity = new Vector2(0, 0);
 
             //プレイヤ―を正しい向きで止める
@@ -398,6 +428,14 @@ public class CPlayerScript : MonoBehaviour
 
             //全てのリジッドボディを止める
             Rbody.constraints = RigidbodyConstraints2D.FreezeAll;
+
+            //飛び始めと比較
+            Vector2 NowPosition = transform.position;
+            var diff = NowPosition - StartPosition;
+            if (diff.magnitude > CConst.FLY_DISTANCE)
+            {
+                Fly = true;
+            }
         }
 
         //********************************************************************
@@ -433,7 +471,7 @@ public class CPlayerScript : MonoBehaviour
         {
             //---宮本加筆ここから------------------------------
             // 大砲発射エフェクト発生
-            GetObject(GunShootEffect, EffectPosition, Quaternion.Euler(GunRotate.x, GunRotate.y, GunRotate.z * 100));
+            GetGunShootObject(GunShootEffect, EffectPosition, Quaternion.Euler(GunRotate.x, GunRotate.y, GunRotate.z * 100));
             //---宮本加筆ここまで------------------------------
 
             SpriteRenderer.color = new Color(1, 1, 1, 1);
@@ -489,6 +527,13 @@ public class CPlayerScript : MonoBehaviour
             StopFieldFlag = true;
             audioSource.PlayOneShot(Player_Sit);        //くっつくSE
         }
+
+        // 壁にぶつかる
+        if (collision.gameObject.tag == "PlayerCrush")
+        {
+            WallFlag = true;
+            WallCount++;
+        }
     }
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -499,7 +544,7 @@ public class CPlayerScript : MonoBehaviour
 
             //---宮本加筆ここから------------------------------
             // 大砲エントリーエフェクト発生
-            GetObject(GunEnterEffect, EffectPosition, Quaternion.Euler(GunRotate.x, GunRotate.y, GunRotate.z * 100));
+            GetGunEnterObject(GunEnterEffect, EffectPosition, Quaternion.Euler(GunRotate.x, GunRotate.y, GunRotate.z * 100));
             //---宮本加筆ここまで------------------------------
         }
 
@@ -509,11 +554,24 @@ public class CPlayerScript : MonoBehaviour
             audioSource.PlayOneShot(Player_GetTrophy);      //トロフィーをとった時のSE
             GetStageTrophy = true;
         }
+
+        //風
+        if (collider.gameObject.name == "Wind")
+        {
+            Wind = true;
+        }
+
+        //コイン
+        //風
+        if (collider.gameObject.name == "Coin")
+        {
+            GetCoin++;
+        }
     }
 
     //---宮本加筆ここから------------------------------
     // // ゲームオブジェクトのアクティブ判別と生成 // //
-    void GetObject(GameObject obj, Vector3 pos, Quaternion qua)
+    void GetPlayerShotObject(GameObject obj, Vector3 pos, Quaternion qua)
     {
         foreach (Transform transform in ShotPool)
         {
@@ -528,6 +586,40 @@ public class CPlayerScript : MonoBehaviour
 
         // 非アクティブなオブジェクトがなければ生成する
         Instantiate(obj, pos, qua, ShotPool);
+
+    }
+    void GetGunEnterObject(GameObject obj, Vector3 pos, Quaternion qua)
+    {
+        foreach (Transform transform in GunEnterPool)
+        {
+            // オブジェクトが非アクティブなら使いまわし
+            if (!transform.gameObject.activeSelf)
+            {
+                transform.SetPositionAndRotation(pos, qua);
+                transform.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        // 非アクティブなオブジェクトがなければ生成する
+        Instantiate(obj, pos, qua, GunEnterPool);
+    }
+
+    void GetGunShootObject(GameObject obj, Vector3 pos, Quaternion qua)
+    {
+        foreach (Transform transform in GunShootPool)
+        {
+            // オブジェクトが非アクティブなら使いまわし
+            if (!transform.gameObject.activeSelf)
+            {
+                transform.SetPositionAndRotation(pos, qua);
+                transform.gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        // 非アクティブなオブジェクトがなければ生成する
+        Instantiate(obj, pos, qua, GunShootPool);
     }
     //---宮本加筆ここまで------------------------------
 }
