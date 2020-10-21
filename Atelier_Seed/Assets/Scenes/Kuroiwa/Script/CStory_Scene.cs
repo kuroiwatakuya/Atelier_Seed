c64b42ca960e4c35f3f3fb4efc06853cacc05bde
kuroiwatakuya <kuroiwa.takuya8975@gmail.com>
2020-10-21 12:39:15 +0900

    黒岩：ストーリーシーン途中

---
+++ Atelier_Seed/Assets/Scenes/Kuroiwa/Script/CStory_Scene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//=================================================
+//ストーリーシーンスクリプト
+//=================================================
+public class CStory_Scene : MonoBehaviour
+{
+    //名前格納
+    public string StageName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //2秒後にシーンチェンジ関数を呼ぶ
+        Invoke("Scene_Change", 2.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Scene_Change()
+    {
+        SceneManager.LoadScene(StageName);
+    }
+}
