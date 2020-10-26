cbbdd5b4fd8d77f7126be11aacc1941160a2e6a3
kuroiwatakuya <kuroiwa.takuya8975@gmail.com>
2020-10-26 18:37:17 +0900

    黒岩：BGM、SE途中、プレイヤーバグ修正

--- Atelier_Seed/Assets/Scenes/Sonfi/BreakBlock.cs
+++ Atelier_Seed/Assets/Scenes/Sonfi/BreakBlock.cs
@@ -18,14 +18,10 @@ public class BreakBlock : MonoBehaviour
 
     private bool BreakFlag;
 
-    [SerializeField] private AudioClip BreakBlock_SE;   //破壊壁SE
-    AudioSource BreakBlocl_Source;
-
     // Start is called before the first frame update
     void Start()
     {
         PlayerScript = Player.GetComponent<CPlayerScript>();
-        BreakBlocl_Source = GetComponent<AudioSource>();
         BreakFlag = false;        
     }
 
@@ -47,7 +43,6 @@ public class BreakBlock : MonoBehaviour
     {
         if (collision.gameObject.name == "Player" && (PlayerScript.Velocity.x >= Power || PlayerScript.Velocity.x >= Power))
         {
-            BreakBlocl_Source.PlayOneShot(BreakBlock_SE);
             BreakFlag = true;
         }
     }
